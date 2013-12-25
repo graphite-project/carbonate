e074cf0332cb20378940dbda781eb536cf9b411b
Scott Sanders <jssjr@github.com>
2013-12-24 19:00:57 -0500

    Make list a generator, add some tests

---
+++ tests/test_list.py
@@ -0,0 +1,39 @@
+import os
+import unittest
+
+from carbonate.list import listMetrics
+
+
+class ListTest(unittest.TestCase):
+
+    metrics_tree = ["foo",
+                    "foo/sprockets.wsp",
+                    "foo/widgets.wsp",
+                    "ham",
+                    "ham/bones.wsp",
+                    "ham/hocks.wsp"]
+
+    expected_metrics = ["foo.sprockets",
+                        "foo.widgets",
+                        "ham.bones",
+                        "ham.hocks"]
+
+    rootdir = os.path.join(os.curdir, 'test_storage')
+
+    @classmethod
+    def setUpClass(cls):
+        os.system("rm -rf %s" % cls.rootdir)
+        os.mkdir(cls.rootdir)
+        for f in cls.metrics_tree:
+            if f.endswith('wsp'):
+                open(os.path.join(cls.rootdir, f), 'w').close()
+            else:
+                os.mkdir(os.path.join(cls.rootdir, f))
+
+    def test_list(self):
+        res = list(listMetrics(self.rootdir))
+        self.assertEqual(res, self.expected_metrics)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.system("rm -rf %s" % cls.rootdir)
