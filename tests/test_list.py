37b355f5afc72edb84f1363dfd49fc8e59ae71ba
Theral Mackey <tmackey at evernote dot com>
2016-06-13 17:08:08 -0700

    Adding test for symlink option

--- tests/test_list.py
+++ tests/test_list.py
@@ -18,6 +18,15 @@ class ListTest(unittest.TestCase):
                         "ham.bones",
                         "ham.hocks"]
 
+    expected_metrics_withsym = ["foo.sprockets",
+                                "foo.widgets",
+                                "ham.bones",
+                                "ham.hocks",
+                                "bar.sprockets",
+                                "bar.widgets"]
+
+    symlinks = [("foo", "bar"),]
+
     rootdir = os.path.join(os.curdir, 'test_storage')
 
     @classmethod
@@ -29,6 +38,15 @@ class ListTest(unittest.TestCase):
                 open(os.path.join(cls.rootdir, f), 'w').close()
             else:
                 os.mkdir(os.path.join(cls.rootdir, f))
+        for s in cls.symlinks:
+            os.symlink(os.path.join(cls.rootdir, s[0]), os.path.join(cls.rootdir, s[1]))
+    def test_list_withsym(self):
+        res = sorted(list(listMetrics(self.rootdir), True))
+        self.assertEqual(res, self.expected_metrics_withsym)
+
+    def test_list_with_trailing_slash_withsym(self):
+        res = sorted(list(listMetrics(self.rootdir + '/', True)))
+        self.assertEqual(res, self.expected_metrics_withsym)
 
     def test_list(self):
         res = sorted(list(listMetrics(self.rootdir)))
@@ -41,3 +59,4 @@ class ListTest(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         os.system("rm -rf %s" % cls.rootdir)
+
