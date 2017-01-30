11a394545d7f3acf3583f0b4ad346d32e82ef410
Corentin Chary <c.chary@criteo.com>
2017-01-30 10:13:48 +0100

    carbonate: support new versions of carbon
    Signed-off-by: Corentin Chary <c.chary@criteo.com>
    Signed-off-by: William Dauchy <w.dauchy@criteo.com>

--- carbonate/cluster.py
+++ carbonate/cluster.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import inspect
 
 # Inject the graphite libs into the system path
 venv_root = ""
@@ -18,7 +19,17 @@ except ImportError as e:
 
 class Cluster():
     def __init__(self, config, cluster='main'):
-        self.ring = ConsistentHashingRouter(config.replication_factor(cluster))
+        # Support multiple versions of carbon, the API changed in 0.10.
+        args = inspect.getargspec(ConsistentHashingRouter.__init__).args
+        if 'replication_factor' in args:
+            ring = ConsistentHashingRouter(config.replication_factor(cluster))
+        else:
+            class Settings(object):
+                REPLICATION_FACTOR = config.replication_factor(cluster)
+                DIVERSE_REPLICAS = False
+            ring = ConsistentHashingRouter(Settings())
+
+        self.ring = ring
 
         try:
             dest_list = config.destinations(cluster)
